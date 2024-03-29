b6eacf3a04f2a31a221e61a906e4bf468a660a1c
Rishikeshari7 <rishikeshari7@gmail.com>
2024-03-29 13:09:07 +0530

    styling done for productItem.jsx

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -6,12 +6,12 @@ const ProductItem = ({item}) => {
   return (
     <div className='ProductItem-cont'>
       <h2>{item.title.substring(0,15)+"..."}</h2>
-      <p>{item.description.substring(0,50)+"..."}</p>
+      <p>{item.description.substring(0,65)+"..."}</p>
       <img className='pr-it-img' src={item.image}></img>
       
       <div className='pr-it-cont-2'>
-        <h4>${item.price}</h4>
-        <button>Add to cart</button> {/* to be modified for add or remove and toast to be added */}
+        <h4 className='pr-it-price'><b className='dolr'>$</b>{item.price}</h4>
+        <button className='pr-it-btn'>Add to cart</button> {/* to be modified for add or remove and toast to be added */}
       </div>
     </div>
   )
